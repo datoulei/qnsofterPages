21afbe6a124ed8dc9644f02d8f92ed7e9852d6ac
datoulei <jiangyunbh@gmail.com>
2014-07-18 16:27:01 +0800

    add qnsofter_v2.0.5.crx

--- cloud/app.js
+++ cloud/app.js
@@ -8,9 +8,10 @@ app.set('view engine', 'ejs');    // 设置 template 引擎
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-// app.get('/hello', function(req, res) {
-//   res.render('hello', { message: 'Congrats, you just set up your app!' });
-// });
+app.post('/hello', function(req, res) {
+  // res.render('hello', { message: 'Congrats, you just set up your app!' });
+  res.send(200,req.body);
+});
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
